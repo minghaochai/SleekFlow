d7663f96d985fd9686759f7975bfce88dc648d77
Minghao Chai <minghao.chai@contractors.roche.com>
2023-06-01 12:49:37 +0800

    chore: remove unused property

--- src/SleekFlow.Application/Common/Dtos/BaseFilterRequest.cs
+++ src/SleekFlow.Application/Common/Dtos/BaseFilterRequest.cs
@@ -4,8 +4,6 @@ namespace SleekFlow.Application.Common.Dtos
 {
     public abstract class BaseFilterRequest<T> : IMapFrom<T>
     {
-        public string[]? Keyword { get; set; }
-
         public string? SortColumn { get; set; }
 
         public string? SortDirection { get; set; }
