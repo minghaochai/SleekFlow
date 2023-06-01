0f9e2dcbdce289777fcb63f0b85456bbcefc2423
Minghao Chai <minghao.chai@contractors.roche.com>
2023-06-01 17:56:46 +0800

    feat: update feature integration tests

--- src/SleekFlow.IntegrationTest/Tests/Features/ToDos/ToDoControllerTest.cs
+++ src/SleekFlow.IntegrationTest/Tests/Features/ToDos/ToDoControllerTest.cs
@@ -15,6 +15,7 @@ namespace SleekFlow.IntegrationTest.Tests.Features.ToDos
     : IClassFixture<CustomWebApplicationFactory<Program>>
     {
         private readonly CustomWebApplicationFactory<Program> _factory;
+        private readonly string _tableName = "[dbo].[ToDos]";
         private readonly List<ToDo> _seedingData = new List<ToDo>()
         {
             new ToDo {
@@ -62,7 +63,7 @@ namespace SleekFlow.IntegrationTest.Tests.Features.ToDos
             {
                 var scopedServices = scope.ServiceProvider;
                 var db = scopedServices.GetRequiredService<SleekFlowDbContext>();
-                Utilities<ToDo>.ReinitializeDb(db, _seedingData, "[dbo].[ToDos]");
+                Utilities<ToDo>.ReinitializeDb(db, _seedingData, _tableName);
             }
             var client = _factory.CreateClient();
 
@@ -79,14 +80,14 @@ namespace SleekFlow.IntegrationTest.Tests.Features.ToDos
         [InlineData("/todos/1")]
         [InlineData("/todos/2")]
         [InlineData("/todos/3")]
-        public async Task GetById_InvalidIds_EndpointsReturnSuccessButCorrectRecords(string url)
+        public async Task GetById_InvalidIds_EndpointsReturnNotFoundAndErrorResponse(string url)
         {
             // Arrange
             using (var scope = _factory.Services.CreateScope())
             {
                 var scopedServices = scope.ServiceProvider;
                 var db = scopedServices.GetRequiredService<SleekFlowDbContext>();
-                Utilities<ToDo>.DropTableRecords(db);
+                Utilities<ToDo>.DropAndRecreateDb(db);
             }
             var client = _factory.CreateClient();
 
@@ -110,7 +111,7 @@ namespace SleekFlow.IntegrationTest.Tests.Features.ToDos
             {
                 var scopedServices = scope.ServiceProvider;
                 var db = scopedServices.GetRequiredService<SleekFlowDbContext>();
-                Utilities<ToDo>.DropTableRecords(db);
+                Utilities<ToDo>.DropAndRecreateDb(db);
             }
             var client = _factory.CreateClient();
             var urlParam = "?user=Test";
@@ -132,7 +133,7 @@ namespace SleekFlow.IntegrationTest.Tests.Features.ToDos
             {
                 var scopedServices = scope.ServiceProvider;
                 var db = scopedServices.GetRequiredService<SleekFlowDbContext>();
-                Utilities<ToDo>.DropTableRecords(db);
+                Utilities<ToDo>.DropAndRecreateDb(db);
             }
             var client = _factory.CreateClient();
             var urlParam = "?user=Test";
@@ -142,6 +143,59 @@ namespace SleekFlow.IntegrationTest.Tests.Features.ToDos
 
             // Assert
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Theory]
+        [InlineData("/todos/1", 0)]
+        [InlineData("/todos/2", 1)]
+        [InlineData("/todos/3", 2)]
+        public async Task Update_ExistingToDo_EndpointsReturnSuccessAndUpdatedRecord(string url, int seedingDataIndex)
+        {
+            // Arrange
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<SleekFlowDbContext>();
+                Utilities<ToDo>.ReinitializeDb(db, _seedingData, _tableName);
+            }
+            var client = _factory.CreateClient();
+            var urlParam = "?user=Test";
+            var updatedToDo = _seedingData[seedingDataIndex];
+            updatedToDo.Name = $"Updated {updatedToDo.Name}";
+
+            // Act
+            var response = await client.PutAsync($"{url}{urlParam}", updatedToDo.CastToHttpContent());
+            var result = response.CastToModel<ToDoResponse>();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal(updatedToDo.Name, result.Name);
+            Assert.Equal("Test", result.EditBy);
+        }
+
+        [Theory]
+        [InlineData("/todos/1", 0)]
+        public async Task Update_ToDoDoesNotExist_EndpointsReturnSuccessAndUpdatedRecord(string url, int seedingDataIndex)
+        {
+            // Arrange
+            using (var scope = _factory.Services.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var db = scopedServices.GetRequiredService<SleekFlowDbContext>();
+                Utilities<ToDo>.DropAndRecreateDb(db);
+            }
+            var client = _factory.CreateClient();
+            var urlParam = "?user=Test";
+            var updatedToDo = _seedingData[seedingDataIndex];
+            updatedToDo.Name = $"Updated {updatedToDo.Name}";
+
+            // Act
+            var response = await client.PutAsync($"{url}{urlParam}", updatedToDo.CastToHttpContent());
+            var result = response.CastToModel<ErrorResponse>();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            Assert.Equal("To do not found", result.Errors![0].Message);
         }
     }
 }
